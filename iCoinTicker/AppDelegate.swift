f2e5059c93279409820dd5f66050c320fcee3dd8
장진우 <arzz@arzz.com>
2017-05-22 22:31:38 +0900

    Initial Commit

---
+++ iCoinTicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iCoinTicker
+//
+//  Created by 알쯔 on 2017. 5. 22..
+//  Copyright © 2017년 Moimz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iCoinTicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iCoinTicker
+//
+//  Created by 알쯔 on 2017. 5. 22..
+//  Copyright © 2017년 Moimz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
